07395f2a8cd72475c8243e053d138fb7ab96948e
root <you@example.com>
2020-09-10 16:59:37 +0000

    .

--- config.php
+++ config.php
@@ -20,18 +20,18 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => password_hash('Rekkun', PASSWORD_DEFAULT),
+    'guest' => password_hash('guest', PASSWORD_DEFAULT)
 );
 
 //set application theme
 //options - 'light' and 'dark'
-$theme = 'light';
+$theme = 'dark';
 
-// Readonly users
+// Readonly users 
 // e.g. array('users', 'guest', ...)
 $readonly_users = array(
-    'user'
+    'guest'
 );
 
 // Enable highlight.js (https://highlightjs.org/) on view's page
